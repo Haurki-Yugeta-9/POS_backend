1c9777bd8db59cbcba806d44507347c9ca358fda
haruki-9 <ygthrsc05@gmail.com>
2025-06-12 05:24:40 +0900

    ローカル用に戻す

--- db_control/connect.py
+++ db_control/connect.py
@@ -22,8 +22,8 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-# SSL証明書のパス
-ssl_cert = str(base_path / "DigiCertGlobalRootCA.crt.pem")
+# # SSL証明書のパス
+# ssl_cert = str(base_path / "DigiCertGlobalRootCA.crt.pem")
 
 # MySQLのURL構築
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
